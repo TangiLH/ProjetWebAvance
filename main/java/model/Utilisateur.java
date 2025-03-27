225695816726521963bf23d7daf69c71c6b42e73
Tangi Le Henaff <lehenafftangi@gmail.com>
2025-03-27 08:35:16 +0100

    added packages

---
+++ main/java/model/Utilisateur.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Utilisateur {
+	private String username;
+	private int id;
+	public Utilisateur(String username, int id) {
+		super();
+		this.username = username;
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+
+}
